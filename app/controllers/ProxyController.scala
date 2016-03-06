10cdb946db9e1532d9c3ff2d2a57fc68dc6e908e
Ian Polding <ipolding@gmail.com>
2016-03-06 20:20:03 +0000

    Add logging

--- app/controllers/ProxyController.scala
+++ app/controllers/ProxyController.scala
@@ -13,7 +13,7 @@ import scala.concurrent.ExecutionContext
  *
  * @param exec We need an `ExecutionContext` to execute our
  * asynchronous code.
-  * @param sd need an `ServiceiDiscovery` to find the URLs of servers ultimately responsible for the request.
+  * @param sd need a `ServiceDiscoverer ` to find the URLs of servers ultimately responsible for the request.
   */
 @Singleton
 class ProxyController @Inject()(sd : ServiceDiscovery, ws : WSClient)(implicit exec: ExecutionContext) extends Controller {
@@ -27,13 +27,19 @@ class ProxyController @Inject()(sd : ServiceDiscovery, ws : WSClient)(implicit e
       Logger.info(s"processing $request")
       val port = if (request.path.contains("playlist")) {":9000"} else {""}
 
-      val url = s"${sd.getHost(request)}${port}${request.path}?${request.rawQueryString}"
+      var url = s"${sd.getHost(request)}${port}${request.path}"
+
+      if (!request.rawQueryString.isEmpty){
+        url = s"${sd.getHost(request)}${port}${request.path}?${request.rawQueryString}"
+      }
+
       Logger.info(s"${request.uri} resolved to $url")
       val response = ws.url(s"http://$url").get()
 
       response.map(
         res => {
           val header: Option[String] = res.header("Content-Type")
+          Logger.info(s"url=$url status = ${res.status}")
           Ok(res.body).withHeaders(("Content-Type", header.get))}
 
       )
